238aceddab7c9202275952123dbdb079733f58fb
Talisson Bruno <talissonbrsouza@gmail.com>
2021-02-23 23:50:12 -0300

    segundo dia nlw4 countdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,22 @@ import { ExperienceBar } from "../components/ExperienceBar";
 
 import styles from '../styles/pages/Home.module.css';
 import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
+
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
       <ExperienceBar />
       <section>
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
         <div>
 
